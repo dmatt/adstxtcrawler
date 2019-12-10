d3fd0e58ab80b54542c97542315859e69948c420
dmatt <dmatt@disqus.com>
2019-12-09 17:40:51 -0800

    Update disqus_adstxt table creation to use 10/14/19 list

--- adstxt_crawler.sql
+++ adstxt_crawler.sql
@@ -13,13 +13,24 @@ CREATE TABLE pub_adstxt(
 );
 
 CREATE TABLE disqus_adstxt(
-       SITE_DOMAIN                  TEXT    NOT NULL,
-       EXCHANGE_DOMAIN              TEXT    NOT NULL,
-       SELLER_ACCOUNT_ID            TEXT    NOT NULL,
-       ACCOUNT_TYPE                 TEXT    NOT NULL,
-       TAG_ID                       TEXT    NOT NULL,
-       ENTRY_COMMENT                TEXT    NOT NULL,
-       UPDATED                      DATE    DEFAULT (datetime('now','localtime'))
+       EXCHANGE_DOMAIN              TEXT    ,
+       SELLER_ACCOUNT_ID            TEXT    ,
+       ACCOUNT_TYPE                 TEXT    ,
+       TAG_ID                       TEXT    ,
 );
+
+INSERT INTO `disqus_adstxt` VALUES ('google.com', 'pub-6650322601660058', 'DIRECT', 'f08c47fec0942fa0');
+INSERT INTO `disqus_adstxt` VALUES ('google.com', 'pub-6650322601660058', 'RESELLER', 'f08c47fec0942fa0');
+INSERT INTO `disqus_adstxt` VALUES ('rubiconproject.com', '13380', 'DIRECT', '0bfd66d529a55807');
+INSERT INTO `disqus_adstxt` VALUES ('rubiconproject.com', '13380', 'RESELLER', '0bfd66d529a55807');
+INSERT INTO `disqus_adstxt` VALUES ('rubiconproject.com', '18798', 'DIRECT', '0bfd66d529a55807'
+INSERT INTO `disqus_adstxt` VALUES ('rubiconproject.com', '18798', 'RESELLER', '0bfd66d529a55807');
+INSERT INTO `disqus_adstxt` VALUES ('appnexus.com', '9733', 'RESELLER');
+INSERT INTO `disqus_adstxt` VALUES ('appnexus.com', '2797', 'DIRECT');
+INSERT INTO `disqus_adstxt` VALUES ('appnexus.com', '1226', 'RESELLER');
+INSERT INTO `disqus_adstxt` VALUES ('appnexus.com', '3364', 'RESELLER');
+INSERT INTO `disqus_adstxt` VALUES ('aol.com', '53392', 'RESELLER');
+INSERT INTO `disqus_adstxt` VALUES ('openx.com', '537133236', 'RESELLER', '6a698e2ec38604c6');
+INSERT INTO `disqus_adstxt` VALUES ('openx.com', '540717801', 'RESELLER', '6a698e2ec38604c6');
 
 COMMIT;
